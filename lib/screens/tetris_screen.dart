44123ab9d48a46e030df9e66f852e3c1532c178b
2breadkey <2breadkey@gmail.com>
2020-10-07 00:27:39 +0900

    fix game screen size

--- lib/screens/tetris_screen.dart
+++ lib/screens/tetris_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:tetris/models/tetris.dart';
@@ -15,6 +16,7 @@ class TetrisScreen extends StatefulWidget {
 
 class _TetrisScreenState extends State<TetrisScreen>
     with SingleTickerProviderStateMixin {
+  static const gameScreenRatio = 4 / 5;
   Tetris tetris;
 
   AnimationController hardDropAnimController;
@@ -59,76 +61,82 @@ class _TetrisScreenState extends State<TetrisScreen>
 
   @override
   Widget build(BuildContext context) {
+    final sizeOfScreen = MediaQuery.of(context).size;
+
+    final height = min(
+            min(sizeOfScreen.width, sizeOfScreen.height) / gameScreenRatio,
+            sizeOfScreen.height) -
+        30;
+
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 8),
       color: antiqueWhite,
       child: Stack(
         children: [
           Align(
-              alignment: Alignment.topCenter,
-              child: AspectRatio(
-                aspectRatio: 5 / 6,
-                child: Container(
-                  margin: EdgeInsets.symmetric(horizontal: 14),
-                  padding: EdgeInsets.only(left: 24, right: 24, bottom: 38),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.vertical(
-                          top: Radius.circular(24),
-                          bottom: Radius.circular(38)),
-                      gradient: LinearGradient(colors: [
-                        Colors.grey[400],
-                        Colors.grey[500],
-                        Colors.grey[600],
-                        Colors.grey[700],
-                      ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-                      boxShadow: [
-                        BoxShadow(color: Colors.grey[800], blurRadius: 14),
-                        BoxShadow(
-                          color: Colors.grey,
-                          spreadRadius: 8,
-                        ),
-                      ]),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SlideTransition(
-                        position: hardDropAnimation,
-                        child: PlayfieldRenderer(tetris),
-                      ),
-                      const VerticalDivider(
-                        color: Colors.transparent,
-                      ),
-                      Expanded(
-                        child: SafeArea(
-                            child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            NextMinoRenderer(tetris),
-                            const Divider(
-                              color: Colors.transparent,
-                            ),
-                            LabelAndNumberRenderer("Level", tetris.levelStream),
-                            const Divider(
-                              color: Colors.transparent,
-                            ),
-                            LabelAndNumberRenderer("Score", tetris.scoreStream),
-                            const Divider(
-                              color: Colors.transparent,
-                            ),
-                            Expanded(
-                              child: Material(
-                                color: neutralBlackC,
-                                elevation: 4,
-                                child: const SizedBox.expand(),
-                              ),
-                            )
-                          ],
-                        )),
-                      )
-                    ],
+            alignment: Alignment.topCenter,
+            child: Container(
+              width: height * gameScreenRatio,
+              height: height,
+              margin: EdgeInsets.symmetric(horizontal: 14),
+              padding: EdgeInsets.only(left: 24, right: 24, bottom: 38),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(24), bottom: Radius.circular(38)),
+                  gradient: LinearGradient(colors: [
+                    Colors.grey[400],
+                    Colors.grey[500],
+                    Colors.grey[600],
+                    Colors.grey[700],
+                  ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+                  boxShadow: [
+                    BoxShadow(color: Colors.grey[800], blurRadius: 14),
+                    BoxShadow(
+                      color: Colors.grey,
+                      spreadRadius: 8,
+                    ),
+                  ]),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SlideTransition(
+                    position: hardDropAnimation,
+                    child: PlayfieldRenderer(tetris),
                   ),
-                ),
-              )),
+                  const VerticalDivider(
+                    color: Colors.transparent,
+                  ),
+                  Expanded(
+                    child: SafeArea(
+                        child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        NextMinoRenderer(tetris),
+                        const Divider(
+                          color: Colors.transparent,
+                        ),
+                        LabelAndNumberRenderer("Level", tetris.levelStream),
+                        const Divider(
+                          color: Colors.transparent,
+                        ),
+                        LabelAndNumberRenderer("Score", tetris.scoreStream),
+                        const Divider(
+                          color: Colors.transparent,
+                        ),
+                        Expanded(
+                          child: Material(
+                            color: neutralBlackC,
+                            elevation: 4,
+                            child: const SizedBox.expand(),
+                          ),
+                        )
+                      ],
+                    )),
+                  )
+                ],
+              ),
+            ),
+          ),
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
