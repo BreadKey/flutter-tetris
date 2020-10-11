770e84765ed5b251894af7fb769d15473fa684f5
2breadkey <2breadkey@gmail.com>
2020-10-11 15:23:26 +0900

    fix audio issue

--- lib/models/audio_manager.dart
+++ lib/models/audio_manager.dart
@@ -16,103 +16,87 @@ enum Effect {
 class AudioManager {
   static final instance = AudioManager._();
 
-  final _bgmPlayers = <Bgm, Future<AudioPlayer>>{};
+  final _bgmPlayer = AudioPlayer();
   final _bgmCache = AudioCache();
+
+  final _effectCache = AudioCache();
 
   bool _isMuted = false;
   bool get isMuted => _isMuted;
 
-  AudioManager._();
+  AudioManager._() {
+    _bgmCache.fixedPlayer = _bgmPlayer;
+  }
 
   void startBgm(Bgm bgm) async {
-    if (_bgmPlayers[bgm] == null) {
-      switch (bgm) {
-        case Bgm.play:
-          _bgmPlayers[bgm] = _bgmCache.loop("audios/tetris-gameboy-02.mp3");
-          break;
-        case Bgm.gameOver:
-          _bgmPlayers[bgm] = _bgmCache.loop("audios/tetris-gameboy-01.mp3");
-          break;
-        default:
-          break;
-      }
+    switch (bgm) {
+      case Bgm.play:
+        _bgmCache.loop("audios/tetris-gameboy-02.mp3");
+        break;
+      case Bgm.gameOver:
+        _bgmCache.loop("audios/tetris-gameboy-01.mp3");
+        break;
+      default:
+        break;
     }
-
-    final bgmPlayer = await _bgmPlayers[bgm];
-    if (_isMuted) {
-      bgmPlayer.setVolume(0);
-    }
-    bgmPlayer.resume();
   }
 
   void stopBgm(Bgm bgm) async {
-    final bgmPlayer = await _bgmPlayers[bgm];
-
-    bgmPlayer?.stop();
+    _bgmPlayer.stop();
   }
 
   void toggleMute() async {
     _isMuted = !_isMuted;
-
-    final bgmPlayers = await Future.wait(_bgmPlayers.values);
-
-    for (AudioPlayer bgmPlayer in bgmPlayers) {
-      if (_isMuted) {
-        bgmPlayer.setVolume(0);
-      } else {
-        bgmPlayer.setVolume(1);
-      }
+    if (_isMuted) {
+      _bgmPlayer.setVolume(0);
+    } else {
+      _bgmPlayer.setVolume(1);
     }
   }
 
   void dispose() {
-    _bgmPlayers.values.forEach((future) {
-      future.then((player) => player.dispose());
-    });
+    _bgmPlayer.dispose();
   }
 
   void pause() {
-    _bgmPlayers.values.forEach((future) {
-      future.then((player) => player.setVolume(0));
-    });
+    _bgmPlayer.pause();
   }
 
   void resume() {
-    _bgmPlayers.values.forEach((future) {
-      future.then((player) {
-        if (!isMuted) {
-          player.setVolume(1);
-        }
-      });
-    });
+    _bgmPlayer.resume();
   }
 
   void playEffect(Effect effect) {
     if (_isMuted) return;
 
+    String effectFile;
+
     switch (effect) {
       case Effect.lock:
-        _bgmCache.play("audios/lock.wav");
+        effectFile = "audios/lock.wav";
         break;
       case Effect.move:
-        _bgmCache.play("audios/move.wav");
+        effectFile = "audios/move.wav";
         break;
       case Effect.rotate:
-        _bgmCache.play("audios/rotate.wav");
+        effectFile = ("audios/rotate.wav");
         break;
       case Effect.hardDrop:
       case Effect.softDrop:
-        _bgmCache.play("audios/hard_drop.wav");
+        effectFile = ("audios/hard_drop.wav");
         break;
       case Effect.breakLine:
-        _bgmCache.play("audios/break_line.wav");
+        effectFile = ("audios/break_line.wav");
         break;
       case Effect.event:
-        _bgmCache.play("audios/event.wav");
+        effectFile = "audios/event.wav";
         break;
       case Effect.levelUp:
-        _bgmCache.play("audios/level_up.wav");
+        effectFile = ("audios/level_up.wav");
         break;
     }
+
+    _effectCache.play(effectFile,
+        mode: PlayerMode.LOW_LATENCY, isNotification: false);
   }
 }
