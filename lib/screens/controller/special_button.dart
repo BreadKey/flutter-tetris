17f89f10c2c9f3e6e5892c7054f615a7637a1bed
2breadkey <2breadkey@gmail.com>
2021-09-23 11:03:42 +0900

    fix special button

--- lib/screens/controller/special_button.dart
+++ lib/screens/controller/special_button.dart
@@ -1,9 +1,9 @@
 part of controller;
 
 class _SpecialButton extends StatelessWidget {
-  final ButtonKey? buttonKey;
+  final ButtonKey buttonKey;
 
-  const _SpecialButton({Key? key, this.buttonKey}) : super(key: key);
+  const _SpecialButton({Key? key, required this.buttonKey}) : super(key: key);
 
   @override
   Widget build(BuildContext context) => Transform.rotate(
@@ -15,12 +15,12 @@ class _SpecialButton extends StatelessWidget {
                 offset: Offset(0, 18),
                 child: ConstrainedBox(
                   constraints: BoxConstraints(minHeight: 24),
-                  child: Controller.of(context).buttonIcons[buttonKey!] ??
+                  child: Controller.of(context).buttonIcons[buttonKey] ??
                       const SizedBox.shrink(),
                 )),
             MaterialButton(
               onPressed: () {
-                Controller.of(context).onButtonEntered?.call(buttonKey!);
+                Controller.of(context).onButtonEntered?.call(buttonKey);
               },
               materialTapTargetSize: MaterialTapTargetSize.padded,
               color: Controller.of(context).specialButtonColor,
